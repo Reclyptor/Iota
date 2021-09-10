097b3ba9ae3755613c34e37390feec3b5d384262
Emilio Torres Jr <emiliotorres3193@gmail.com>
2021-09-10 01:19:13 -0500

    Bootstrapped SpringBoot Project

---
+++ src/main/java/com/walmart/iota/IotaApplication.java
@@ -0,0 +1,11 @@
+package com.walmart.iota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IotaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/walmart/iota/IotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.iota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
