021bef12f7f6f859e2a22006455ecb33e52ef68b
Emilio Torres Jr <emiliotorres3193@gmail.com>
2021-09-12 20:49:24 -0500

    Updated Default Run Parameters / Code Cleanup

--- src/main/java/com/walmart/iota/batch/RefrigeratedTruckTelemetryRecordingConfiguration.java
+++ src/main/java/com/walmart/iota/batch/RefrigeratedTruckTelemetryRecordingConfiguration.java
@@ -19,9 +19,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Date;
